ea0047a01f94c6826945e5400dfed153732a0cf1
DARK-NOVA-6 <111617715+DARK-NOVA-6@users.noreply.github.com>
2023-03-22 02:05:47 +0300

    third task number of students who have failed the course who are also older than 20 years old and inactive.

--- src/main/java/edu/coursera/parallel/StudentAnalytics.java
+++ src/main/java/edu/coursera/parallel/StudentAnalytics.java
@@ -1,10 +1,7 @@
 package edu.coursera.parallel;
 
 import java.util.*;
-import java.util.function.Function;
 import java.util.stream.Collectors;
-import java.util.stream.DoubleStream;
-import java.util.stream.Stream;
 
 
 /**
@@ -157,7 +154,7 @@ public final class StudentAnalytics {
     }
 
     /**
-     * TODO compute the number of students who have failed the course who are
+     * compute the number of students who have failed the course who are
      * also older than 20 years old. A failing grade is anything below a 65. A
      * student has only failed the course if they have a failing grade and they
      * are not currently active. This should mirror the functionality of
@@ -169,6 +166,9 @@ public final class StudentAnalytics {
      */
     public int countNumberOfFailedStudentsOlderThan20ParallelStream(
             final Student[] studentArray) {
-        throw new UnsupportedOperationException();
+        return Math.toIntExact(Arrays.stream(studentArray)
+                .parallel()
+                .filter(s -> !s.checkIsCurrent() && s.getAge() > 20 && s.getGrade() < 65)
+                .count());
     }
 }
